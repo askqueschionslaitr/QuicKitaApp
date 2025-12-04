0337fa32f68545673641ab87f6bba430a09e1777
Vince Lim <vincelim855@gmail.com>
2025-12-04 10:50:53 +0800

    Nitpick change sa comment

--- App.js
+++ App.js
@@ -57,12 +57,12 @@ function TabNavigator() {
       
       <Tab.Screen name="Home" component={DashboardScreen} />
       
-      {/* Find Gigs that's only available for Workers */}
+      {/* 'Find Gigs' only available for Workers */}
       {userRole === 'Worker' && (
         <Tab.Screen name="Find Gigs" component={FindJobsScreen} />
       )}
       
-      {/* Post Gig that's only available for Employers */}
+      {/* 'Post Gig' only available for Employers */}
       {userRole === 'Employer' && (
         <Tab.Screen name="Post Gig" component={PostJobScreen} />
       )}
